c1bbd0b82f041c2f3856440cd2a512100c5e22b9
syukoGit <syukodev@outlook.com>
2022-12-05 18:03:38 -0500

    Add LoginController with his DTOs

---
+++ MacroMinder/Shared/Authentication/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace MacroMinder.Shared.Authentication;
+
+using System.ComponentModel.DataAnnotations;
+
+public class LoginDTO
+{
+    [Required]
+    public required string Password { get; set; }
+
+    public bool RememberMe { get; set; }
+
+    [Required]
+    public required string UserName { get; set; }
+}
---
+++ MacroMinder/Shared/Authentication/LoginResultDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MacroMinder.Shared.Authentication;
+
+public class LoginResultDTO
+{
+    public string? Error { get; set; }
+
+    public bool Successful { get; set; }
+
+    public string? Token { get; set; }
+}
