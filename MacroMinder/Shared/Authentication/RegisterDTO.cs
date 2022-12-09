a45988bb1a23505a34672f70578559738d1fbc79
syukoGit <syukodev@outlook.com>
2022-12-09 11:33:02 -0500

    Add Register page

--- MacroMinder/Shared/Authentication/RegisterDTO.cs
+++ MacroMinder/Shared/Authentication/RegisterDTO.cs
@@ -1,18 +1,20 @@
 ﻿namespace MacroMinder.Shared.Authentication;
 
+using JetBrains.Annotations;
 using System.ComponentModel.DataAnnotations;
 
 public class RegisterDTO
 {
     [DataType(DataType.Password)]
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    public required string ConfirmPassword { get; set; }
+    [UsedImplicitly]
+    public required string ConfirmPassword { get; init; }
 
     [Required]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
     [DataType(DataType.Password)]
-    public required string Password { get; set; }
+    public required string Password { get; init; }
 
     [Required]
-    public required string UserName { get; set; }
+    public required string Username { get; init; }
 }
