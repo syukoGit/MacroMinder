34a9edec57c6a19ad5dd250bb19f9732e18945f3
syukoGit <syukodev@outlook.com>
2022-12-10 16:20:47 -0500

    Add Automapper

---
+++ MacroMinder/Server/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace MacroMinder.Server.Helpers;
+
+using AutoMapper;
+using JetBrains.Annotations;
+using MacroMinder.Server.Entities;
+using MacroMinder.Shared.Food;
+
+public class AutoMapperProfile : Profile
+{
+    [UsedImplicitly]
+    public AutoMapperProfile()
+    {
+        CreateMap<Food, FoodDTO>();
+        CreateMap<CreateFoodDTO, Food>();
+    }
+}
